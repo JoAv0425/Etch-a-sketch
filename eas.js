7e3e80be115ba81284a3100c4b0f40354993eec6
JoAv0425 <praisecandomo@gmail.com>
2026-03-10 13:40:48 +0100

    Create button function

--- eas.js
+++ eas.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('#container');
 
+
+
 for (let i = 0; i < 256; i++) {
     const cellDiv = document.createElement('div');
     cellDiv.classList.add('cellDiv');
@@ -13,3 +15,14 @@ const mouseOver = function (e) {
 };
 
 container.addEventListener('mouseover', mouseOver);
+
+const button = document.createElement('button');
+button.classList.add('btn');
+let noOfSquares;
+const popUp  = function () {
+    noOfSquares = prompt('Number of squares per side of grid', 'max 100');
+    if (noOfSquares > 100 || noOfSquares < 16) {
+        noOfSquares = 16
+    } 
+    return noOfSquares;
+};
