b7622054f4101da8f42e4ead3cf78d12689ab1eb
JoAv0425 <praisecandomo@gmail.com>
2026-03-11 11:12:50 +0100

    Begin implementation of button function

--- eas.js
+++ eas.js
@@ -1,12 +1,36 @@
 const container = document.querySelector('#container');
+const body = document.querySelector('body');
+const button = document.createElement('button');
+button.textContent = "Grid Size";
+button.classList.add('btn');
+button.id = 'popUp';
+body.appendChild(button);
+
+let noOfSquareSides;
+let noOfSquares = 256;
+
+const popUp  = function (e) {
+    noOfSquareSides = prompt('Input number of squares per side of grid', 'max 100');
+    if (noOfSquareSides > '100' ) {
+        noOfSquareSides = '100';
+    } else if (noOfSquareSides < '16' || !noOfSquareSides) {
+        noOfSquareSides = '16';
+    }
+   noOfSquares = ((+noOfSquareSides) ** 2);
 
 
-
-for (let i = 0; i < 256; i++) {
+ for (let i = 0; i < noOfSquares; i++) {
     const cellDiv = document.createElement('div');
     cellDiv.classList.add('cellDiv');
+    cellDiv.style.cssText = `flex: 0 0 ${(100 / noOfSquareSides)};`
     container.appendChild(cellDiv);
-};
+ };
+   
+}
+
+
+button.addEventListener('click', popUp);
+
 
 const mouseOver = function (e) {
    if (e.target.classList.contains('cellDiv')) {
@@ -16,13 +40,5 @@ const mouseOver = function (e) {
 
 container.addEventListener('mouseover', mouseOver);
 
-const button = document.createElement('button');
-button.classList.add('btn');
-let noOfSquares;
-const popUp  = function () {
-    noOfSquares = prompt('Number of squares per side of grid', 'max 100');
-    if (noOfSquares > 100 || noOfSquares < 16) {
-        noOfSquares = 16
-    } 
-    return noOfSquares;
-};
+
+
