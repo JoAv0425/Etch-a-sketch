b67be061ce904c2cf19db802fbc34c5dd80048f1
JoAv0425 <praisecandomo@gmail.com>
2026-03-12 09:42:50 +0100

    Make the 'size' variable a live variable during click event

--- eas.js
+++ eas.js
@@ -10,24 +10,9 @@ let noOfSquareSides = 16;
 let noOfSquares = 256;
 let size = (100 / noOfSquareSides);
 
-const popUp  = function (e) {
-    noOfSquareSides = parseInt(prompt('Input number of squares per side of grid', 'max 100'));
-    if (noOfSquareSides > 100 ) {
-        noOfSquareSides = 100;
-    } else if (noOfSquareSides < 16 || !noOfSquareSides) {
-        noOfSquareSides = 16;
-    }
-   noOfSquares = ((noOfSquareSides) ** 2);
-
-   //create function for clearing old grid and a loop for the new one.
-
-}
-
-
-button.addEventListener('click', popUp);
-
 function gridMap() {
 for (let i = 0; i < noOfSquares; i++) {
+    
     const cellDiv = document.createElement('div');
     cellDiv.classList.add('cellDiv');
     cellDiv.style.flex = `0 0 ${size}%`
@@ -36,7 +21,28 @@ for (let i = 0; i < noOfSquares; i++) {
 };
 }
 
+function cleargridMap() {
+    container.innerHTML = ''; 
+}
+
 gridMap();
+
+const popUp  = function (e) {
+    noOfSquareSides = parseInt(prompt('Input number of squares per side of grid', 'max 100'));
+    if (noOfSquareSides > 100 ) {
+        noOfSquareSides = 100;
+    } else if (noOfSquareSides < 16 || !noOfSquareSides) {
+        noOfSquareSides = 16;
+    }
+   noOfSquares = ((noOfSquareSides) ** 2);
+    size = (100 / noOfSquareSides);
+   //create function for clearing old grid and a loop for the new one.
+   cleargridMap(); 
+   gridMap();
+}
+
+
+button.addEventListener('click', popUp);
 
 const mouseOver = function (e) {
    if (e.target.classList.contains('cellDiv')) {
